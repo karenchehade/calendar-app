5f0ef20094e029813478e77cace9de2f8d9584d8
karen chehade <karen chehade>
2022-06-13 10:00:53 +0300

    work from home push

---
+++ lib/pages/calendar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Calendar extends StatelessWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Text('calendar')),
+    );
+  }
+}
